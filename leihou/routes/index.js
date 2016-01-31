3338e53642ad4597b48bd99ba7a700ce2c77cef7
heavencme <heavencme@gmail.com>
2016-02-01 01:34:50 +0800

    static file restyle

--- leihou/routes/index.js
+++ leihou/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/hongbao', function(req, res, next) {
+    var ua = req.headers['user-agent'].toLowerCase();
+
+    console.log(ua);
+
+    res.render('index', { title: 'Express' });
 });
+
+router.post('/hongbao', function(req, res, next) {
+    var ua = req.headers['user-agent'].toLowerCase();
+    
+
+});
+
 
 module.exports = router;
