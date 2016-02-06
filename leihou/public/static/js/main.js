65ad71ea0732f4aa877702e1bf1c8f6034cade8f
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-02-06 13:43:06 +0800

    materializecss index ok

---
+++ leihou/public/static/js/main.js
@@ -0,0 +1,22 @@
+(function($){
+/* start */
+
+$('.modal-trigger').leanModal();
+
+
+/*
+$('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { console.log('Ready'); }, // Callback for Modal open
+      complete: function() { console.log('Closed'); } // Callback for Modal close
+    }
+);
+
+*/
+
+/** end **/
+})(jQuery);
+
