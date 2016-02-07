a66e6ffa2813e1b0d0eb18859b0fd17b4c50a113
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-02-07 13:19:50 +0800

    remove/add effects on homepage

--- leihou/public/static/js/main.js
+++ leihou/public/static/js/main.js
@@ -1,8 +1,8 @@
 (function($){
 /* start */
 
+/* index modal effect*/
 $('.modal-trigger').leanModal();
-
 
 /*
 $('.modal-trigger').leanModal({
@@ -14,8 +14,33 @@ $('.modal-trigger').leanModal({
       complete: function() { console.log('Closed'); } // Callback for Modal close
     }
 );
-
 */
+
+/* ripple effect */
+$(".ripple-btn").click(addRippleEffect);
+function addRippleEffect(e) {
+    var target = e.target;
+    if (target.tagName.toLowerCase() !== 'div') {
+      return ;
+    }
+    var rect = target.getBoundingClientRect();
+    var ripple = target.querySelector('.ripple');
+    if (!ripple) {
+        ripple = document.createElement('span');
+        ripple.className = 'ripple';
+        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+        target.appendChild(ripple);
+    }
+    ripple.classList.remove('show');
+    var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    ripple.style.top = top + 'px';
+    ripple.style.left = left + 'px';
+    ripple.classList.add('show');
+    return false;
+}
+
+
 
 /** end **/
 })(jQuery);
