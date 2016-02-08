39b09cec0678267df3bf56de8558bc8671b49a94
王彬 <wangbin@wangbindeMacBook-Air.local>
2016-02-09 00:38:04 +0800

    replace cookie functions that not functioning with newly found

--- leihou/public/static/js/main.js
+++ leihou/public/static/js/main.js
@@ -236,27 +236,30 @@ function sendInfoValidate() {
 }
 
 /* set cookie and its expiredays*/
-function setCookie(c_name,c_value,expiredays) {
-    
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + expiredays);
-    document.cookie = c_name + "=" + escape(c_value) + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString()) + ";path=/;domain=.kongzhong.com;secure";
+function setCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";    
 }
 
 /* get cookie */
-function getCookie(c_name) {
-    if(document.cookie.length > 0) {
-        var c_start = document.cookie.indexOf(c_name + "="); 
-        if(c_start != -1) {
-            c_start = c_start + c_name.length + 1; 
-            var c_end = document.cookie.indexOf(";",c_start);
-            if(c_end == -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
-    return "";
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
 }
 
 
